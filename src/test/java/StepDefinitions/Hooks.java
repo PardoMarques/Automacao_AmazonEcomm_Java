343078e20a88a3f8a7cd78560da4de2f616c7286
Caio Marques <caio_asm@hotmail.com>
2024-10-22 14:01:56 -0300

    temp

--- src/test/java/StepDefinitions/Hooks.java
+++ src/test/java/StepDefinitions/Hooks.java
@@ -2,20 +2,44 @@ package StepDefinitions;
 
 import Core.DriverFactory;
 import io.cucumber.java.Scenario;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import org.junit.jupiter.api.*;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.Status;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+import com.aventstack.extentreports.reporter.configuration.Theme;
+//import io.github.bonigarcia.wdm.WebDriverManager;
+
+import java.io.IOException;
 
 public class Hooks {
+	private static ExtentSparkReporter sparkReporter;
+	private static ExtentReports extent;
+	private static ExtentTest extentTest;
 
-	@Before
-	public void InitializeTest() {
-		System.out.println("Abrindo o navegador");
+	@BeforeAll
+	public static void beforeAll() throws IOException {
+		extent = new ExtentReports();
+		sparkReporter = new ExtentSparkReporter(System.getProperty("user.dir") +"\\test-output\\testReport.html");
+		extent.attachReporter(sparkReporter);
+
+		sparkReporter.config().setOfflineMode(true);
+		sparkReporter.config().setDocumentTitle("Simple Automation Report");
+		sparkReporter.config().setReportName("Test Report");
+		sparkReporter.config().setTheme(Theme.STANDARD);
+		sparkReporter.config().setTimeStampFormat("EEEE, MMMM dd, yyyy, hh:mm a '('zzz')'");
+		sparkReporter.config().setEncoding("UTF-8");
 	}
-	
-	@After
+
+	@AfterAll
 	public void TearDownTest (Scenario scenario) {
+		System.out.println("Cenário: " + scenario.getName());
 		if(scenario.isFailed() == true) {
-			System.out.println("Cenário: " + scenario.getName());
+			extentTest.log(Status.PASS, "Teste aprovado");
+		}
+		else{
+			extentTest.log(Status.FAIL, "Teste reprovado");
 		}
 		System.out.println("Fechando o navegador");
 		DriverFactory.killDriver();
